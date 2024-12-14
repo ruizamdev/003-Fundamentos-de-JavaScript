abe1f60bb74db6b75c90bae68c96094cb4b80e27
Armando Ruiz <artmx@proton.me>
2024-12-14 15:03:50 -0600

    Se avanzó modulo 1 de JS Math&Stats. solo falta un ejercicio para terminarlo.

--- 03-Taller-Practico-de-JS-matematicas-y-estadistica-basica/Practica/math.js
+++ 03-Taller-Practico-de-JS-matematicas-y-estadistica-basica/Practica/math.js
@@ -1,32 +1,12 @@
-# Mathematics in JavaScript
-## Formulas Matemáticas en JavaScript
-### Calculando el area y el perímetro de un cuadrado y un triangulo.
-
-#### Cuadrado
-```
-Para obtener el PERÍMETRO de un cuadrado solo debemos de sumar los 4 lados, 
-por lo que podemos simplemente multiplicar uno de los lados por 4:
-
-l x 4 = P
-
-Para obtener el ÁREA del mismo, tenemos que multiplicar un lado por otro
-
-l x l = A
-```
-
-Por lo que podríamos 
-
-```javascript
 // CUADRADO
+// 
 console.group("Cuadrados");
     // medida del lado de un cuadrado
     const ladoCuadrado = 5;
-    // perímetro de un cuadrado
+    // perimetro de un cuadrado
     const perimetroCuadrado = ladoCuadrado * 4;
     // area de un cuadrado
     const areaCuadrado = ladoCuadrado * ladoCuadrado;
-    // también podríamos aplicarlo con exponente al cuadrado
-    // const areaCuadrado = ladoCuadrado ** 2;
     // output en consola
     console.log(
         ladoCuadrado,
@@ -36,22 +16,8 @@ console.group("Cuadrados");
     // output
     // 5 20 25
 console.groupEnd("Cuadrados");
-```
 
-#### Triangulo
 
-```
-En el caso del triangulo para el PERIMETRO, 
-utilizamos la misma formula de lado + lado + lado:
-
-l + l + l = P
-
-Y para el AREA, la formula base por altura sobre 2:
-
-(b * h) / 2
-```
-
-```javascript
 // TRIANGULO
 console.group("Triangulos");
     const ladoTriangulo1 = 6;
@@ -71,13 +37,7 @@ console.group("Triangulos");
         areaTriangulo,
     );
 console.groupEnd("Trinagulos");
-```
 
-<br>
-
-
-Funciones:
-```javascript
 // FUNCION PARA CALCULAR PERIMETRO Y AREA DE UN TRIANGULO
 function calcularTriangulo(lado1, lado2, base, altura) {
     return {
@@ -93,28 +53,8 @@ function calcularCuadrado(lado) {
         area: lado ** 2,
     }
 }
-```
 
-## Math en JavaScript
-### Calculando el perímetro y  
 
-```
-Para calcular el perímetro de un circulo debemos tomar en cuenta dos cosas, el radio,
-el cual es la linea que va desde el centro a cualquier punto exterior del circulo.
-
-radio = r
-
-duplicando el radio y manteniendo las dos partes continuas 
-en una linea recta tenemos lo que es el diámetro.
-
-diámetro = r * 2
-
-Teniendo ya en cuenta estos dos, el perímetro de un circulo (al cual llamamos circunferencia), 
-es el diámetro por pi
-(r * 2) * PI
-```
-
-```javascript
 // CIRCULO
 console.group("Circulos");
 
@@ -134,10 +74,7 @@ console.group("Circulos");
     })
 
 console.groupEnd("Circulos");
-```
 
-Función:
-```javascript
 function calcularCirculo(radio) {
     const diametro = radio * 2;
     const radioAlCuadrado = radio ** 2;
@@ -147,18 +84,20 @@ function calcularCirculo(radio) {
         area: radioAlCuadrado * PI,
     }
 }
-```
 
-<br>
 
-## Reto: Altura de un Triangulo
+/* function calcularAlturaTriangulo(lado1, lado2, base){
+    if (lado1 != lado2 || base == lado1 || base == lado2) {
 
-### Requisitos:
-- Sabiendo lo que miden los 3 lados, encontrar la altura.
-- Debe ser un triangulo isosceles no equilátero (2 lados iguales y 1 diferente.)
+    } else {
 
-Formula:
-```
-h =  raíz cuadrada de (a^2 - b^2/4)
-```
+    }
+} */
 
+function calcularAlturaTriangulo(lado, base) {
+    if (lado == base) {
+        console.warn('Esto no es un triangulo isosceles');
+    } else {
+        return Math.sqrt((lado ** 2) - ((base ** 2)) / 4);
+    }
+}
