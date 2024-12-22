104c03891e7ac95ee1f23c36e485e97b1167bc01
Armando Ruiz <artmx@proton.me>
2024-12-21 19:39:09 -0600

    batallando con el curso 03 math
    no puedo mostrar en pantalla los resultados. aparecen y se van en un instante.

--- 03-Taller-Practico-de-JS-matematicas-y-estadistica-basica/Practica/porcentaje.js
+++ 03-Taller-Practico-de-JS-matematicas-y-estadistica-basica/Practica/porcentaje.js
@@ -1,50 +1,44 @@
-/* let p = prompt('Cual es el precio del producto?');
-let d = prompt('Que descuento tiene?') ;
+// CONSTANTS
+const inputPrice = document.getElementById('inputPrice'); // Precio de netrada
+const inputCupon = document.getElementById('inputCupon'); // Cupon de entrada
+const submitButton = document.getElementById('submitButton'); // Boton de enviar información.
+const divResult = document.getElementById('divResult'); // Tag div para mostrar resultado al usaurio.
 
+submitButton.addEventListener('click', calcularPrecioConDescuento); 
 
-
-function getPriceWithDiscount(p,d){
-    const precioConDescuento = (p * (100 - d))/100;
-    alert(
-        `
-        Este es el Precio con descuento:
-        ${precioConDescuento}
-        `
-    )
+const cupons = {
+    'riquillo': 92,
+    'piky': 90,
+    'rambo': 75,
+    'armandoMariscalRuiz': 65,
+    'silverioDurango': 50,
+    'ruiz7am': 48,
+    'armandoRuiz': 35,
 }
 
-getPriceWithDiscount(p,d) */
-
-document.getElementById('discountForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const price = parseFloat(document.getElementById('price').value);
-    const cupon = document.getElementById('cupon').value;
-
-    let discount;
-    switch (cupon){
-        case `ruiz7am`:
-            discount = 28
-            break;
-        case `javascript`:
-            discount = 23
-            break;
-        case `vanilla.js`:
-            discount = 18
-            break;
-        default:
-            alert('El cupon no es valido')
-            return;
+function calcularPrecioConDescuento(){
+    const price = Number(inputPrice.value);
+    const cupon = inputCupon.value;
+    
+    if (!price || !cupon) {
+        divResult.innerHTML = 'Llena el formulario caraverga';
+        return;
     }
-
-    const moneyDiscounted = (discount * price)/100;
-    const priceWithDiscount = price - moneyDiscounted;
-    const resultados = document.getElementById('resultados');
-    resultados.innerHTML = `
-        <h2>Resultados</h2>
-        <p>El precio del producto es: $${price}</p>
-        <p>El descuento es de: ${discount}%</p>
-        <p>El descuento en dinero es de: $${moneyDiscounted}</p>
-        <p>El precio con descuento es de: $${priceWithDiscount}</p>
-    `
-})
+    let discount;
+    if (cupons[cupon]){
+        discount = cupons[cupon];
+        const moneyDiscounted = (discount * price)/100;
+        const priceWithDiscount = price - moneyDiscounted;
+        divResult.innerHTML = ` 
+            <p>Gracias por usar mi programa</p>
+            <h1>Checa el descuento que obtendras con tu cupon</h1>
+            <p>El codigo de tu cupon es: ${cupon}</p>
+            <p>El descuento que obtines es de: ${discount}%</p>
+            <p>El precio del producto que elegiste es de $${price}</p>
+            <p>El monto del descuento es: $${moneyDiscounted}</p>
+            <p>El precio final de tu producto es de : $${priceWithDiscount}</p>
+        `
+    } else {
+        divResult.innerHTML = `El cupon ${cupon} no es valido`
+    }
+}
