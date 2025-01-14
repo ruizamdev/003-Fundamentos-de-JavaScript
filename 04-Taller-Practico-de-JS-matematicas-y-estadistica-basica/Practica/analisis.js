26c50227947fd29735cfd521ce39440990414d71
Armando Ruiz <artmx@proton.me>
2025-01-14 15:31:10 -0600

    Update
    Terminamos curso de Math JS, :D , pero sacamos 8.8 en el examen, no pase, esperamos a mas tarde o manana para volver a intentar.

--- 04-Taller-Practico-de-JS-matematicas-y-estadistica-basica/Practica/analisis.js
+++ 04-Taller-Practico-de-JS-matematicas-y-estadistica-basica/Practica/analisis.js
@@ -37,7 +37,10 @@ function proyeccionPorPersona (nombrePersona) {
     }
     const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
 
-    const ultimoSalario = trabajos
+    const ultimoSalario = trabajos[trabajos.length - 1].salario;
+    const aumento = ultimoSalario * medianaPorcentajesCrecimiento;
+    const nuevoSalario = ultimoSalario + aumento;
+    return nuevoSalario;
 };
 
 // Analisis Empresarial
@@ -63,4 +66,58 @@ function medianaEmpresaYear(nombre, year) {
     } else {
         return PlatziMath.calcularMediana(empresas[nombre][year])
     }
+}
+
+function proyeccionPorEmpresa (nombre) {
+    if(!empresas[nombre]) {
+        console.warn('La empresa no existe');
+    } else {
+        const empresaYears = Object.keys(empresas[nombre]);
+        const listaMedianaYears = empresaYears.map((year) => {
+            return medianaEmpresaYear(nombre, year);
+        });
+        let porcentajesCrecimiento = [];
+
+        for (let i = 1; i < listaMedianaYears.length; i++){
+            const salarioActual = listaMedianaYears[i];
+            const salarioPasado = listaMedianaYears[i - 1];
+            const crecimiento = salarioActual - salarioPasado;
+            const porcentajeCrecimiento = (crecimiento / salarioPasado)
+            porcentajesCrecimiento.push(porcentajeCrecimiento);
+        }
+
+        const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+
+        const ultimaMediana = listaMedianaYears[listaMedianaYears.length - 1];
+        const aumento = ultimaMediana * medianaPorcentajesCrecimiento;
+        const nuevaMediana = ultimaMediana + aumento;
+
+        return nuevaMediana;
+    };
+};
+
+// Analisis general
+function medianaGeneral () {
+    const listaMedianas = salarios.map(
+        persona => medianaPorPersona(persona.name)
+    );
+    const mediana = PlatziMath.calcularMediana();
+
+    return mediana;
+}
+
+function medianaTop10 () {
+    const listaMedianas = salarios.map(
+        persona => medianaPorPersona(persona.name)
+    );
+
+    const medianasOrdenadas = PlatziMath.ordenarLista(listaMedianas);
+    
+    const cantidad = listaMedianas.length / 10;
+    const limite = listaMedianas.length - cantidad;
+
+    const top10 = medianasOrdenadas.slice(limite, medianasOrdenadas.length);
+    console.log({top10})
+
+    console.log({medianasOrdenadas})
 }
